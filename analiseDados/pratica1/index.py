7d0ca53aa4c626391d7ec7ff875df79e487bf478
HeitorDalla <heitorvillavilla@gmail.com>
2025-05-28 14:35:35 -0300

    Finalizado a pergunta 2, 4, 5, 6. Refatorado o codigo, mudando os graficos e textos

--- analiseDados/pratica1/index.py
+++ analiseDados/pratica1/index.py
@@ -20,20 +20,13 @@ df = df[df['Status'] == 'Developing']
 st.header("Estatísticas sobre a expectativa de vida dos Países")
 
 
-st.subheader('1. Os vários fatores de previsão inicialmente escolhidos realmente afetam a expectativa devida?') 
+st.subheader('1. Os vários fatores de previsão inicialmente escolhidos realmente afetam a expectativa devida?', divider='grey') 
 
 
-st.subheader('2. Um país com menor expectativa de vida (<65) deve aumentar seus gastos com saúde para melhorar sua expectativa de vida média?')
+st.subheader('2. Um país com menor expectativa de vida (<65) deve aumentar seus gastos com saúde para melhorar sua expectativa de vida média?', divider='grey')
 
 # Média da expectativa de vida dos países que tem a média de expectativa de vida menor que 65
 # Expectativa de vida ao longo dos anos
-fig, ax = plt.subplots()
-sns.lineplot(x='Year', y='Life expectancy', data=df, legend=False, ax=ax)
-plt.title("Espectativa de vida por país ao longo dos anos")
-plt.xlabel("Ano")
-plt.ylabel("Espectativa de vida")
-st.pyplot(fig)
-
 porPais = df.groupby('Country')['Life expectancy'].mean().sort_values()
 menor65 = porPais[porPais < 65]
 
@@ -45,6 +38,13 @@ menor65Linhas['percentage expenditure'] = (
     .str.replace(',', '.')
     .astype(float)
 )
+
+fig, ax = plt.subplots()
+sns.lineplot(x='Year', y='Life expectancy', data=df, legend=False, ax=ax)
+plt.title("Espectativa de vida por país ao longo dos anos")
+plt.xlabel("Ano")
+plt.ylabel("Espectativa de vida")
+st.pyplot(fig)
 
 mediaExpectativaMenorPaises65 = menor65Linhas['percentage expenditure'].mean()
 st.write("A média da expectativa de vida dos países que possuem a média de gastos menor que 65 é: {:.2f}" .format(mediaExpectativaMenorPaises65))
@@ -70,27 +70,56 @@ st.write("Os dados mostram que os países com expectativa de vida maior que 65 a
 st.write('\n')
 
 
-st.subheader('3. Como as taxas de mortalidade infantil e adulta afetam a expectativa de vida?')
+st.subheader('3. Como as taxas de mortalidade infantil e adulta afetam a expectativa de vida?', divider='grey')
 
 
-st.subheader('4. A expectativa de vida tem correlação positiva ou negativa com hábitos alimentares, estilo de vida, exercícios, fumo, consumo de álcool etc.')
-# Alcohol
+st.subheader('4. A expectativa de vida tem correlação positiva ou negativa com hábitos alimentares, estilo de vida, exercícios, fumo, consumo de álcool etc.', divider='grey')
+
+# Alcool
 correlacaoAlcoolMortalidadeAdulta = df[['Alcohol', 'Adult Mortality']].corr()
 st.write("O coeficiente de correlação entre o consumo de alcool e a taxa de mortalidade adulta é: ")
 st.write(correlacaoAlcoolMortalidadeAdulta)
 
+fig1, ax1 = plt.subplots()
+sns.regplot(x='Alcohol', y='Adult Mortality', data=df, ax=ax1)
+plt.title("Dispersão entre o consumo de alcool e a mortalidade adulta")
+plt.xlabel("Alcool")
+plt.ylabel("Mortalidade adulta")
+st.pyplot(fig1)
+
 correlacaoAlcoolMortalidadeInfantil = df[['Alcohol', 'infant deaths']].corr()
 st.write("O coeficiente de correlação entre o consumo de alcool e a taxa de mortalidade infantil é: ")
 st.write(correlacaoAlcoolMortalidadeInfantil)
+
+fig2, ax2 = plt.subplots()
+sns.regplot(x='Alcohol', y='infant deaths', data=df, ax=ax2)
+plt.title("Dispersão entre o consumo de alcool e a mortalidade infantil")
+plt.xlabel("Alcool")
+plt.ylabel("Mortalidade infantil")
+st.pyplot(fig2)
 
 # Hepatitis B
 correlacaoHepatitisAdulto = df[['Hepatitis B', 'Adult Mortality']].corr()
 st.write("O coeficiente de correlação entre a Hepatitis B   e a taxa de mortalidade adulta é: ")
 st.write(correlacaoHepatitisAdulto)
 
+fig3, ax3 = plt.subplots()
+sns.regplot(x='Alcohol', y='Adult Mortality', data=df, ax=ax3)
+plt.title("Dispersão entre a Hepatitis B e a mortalidade adulta")
+plt.xlabel("Alcool")
+plt.ylabel("Mortalidade adulta")
+st.pyplot(fig3)
+
 correlacaoHepatitisInfantil = df[['Hepatitis B', 'infant deaths']].corr()
 st.write("O coeficiente de correlação entre a Hepatitis B e a taxa de mortalidade infantil é: ")
 st.write(correlacaoHepatitisInfantil)
+
+fig4, ax4 = plt.subplots()
+sns.regplot(x='Alcohol', y='infant deaths', data=df, ax=ax4)
+plt.title("Dispersão entre a Hepatitis B e a mortalidade infantil")
+plt.xlabel("Alcool")
+plt.ylabel("Mortalidade infantil")
+st.pyplot(fig4)
 
 st.write('Conclusões da pergunta 4:')
 st.write('As correlações entre o consumo de alcool e a hepatitis b com as mortalidades adulta e infantil mostram que' \
@@ -99,18 +128,19 @@ st.write('As correlações entre o consumo de alcool e a hepatitis b com as mort
 st.write('\n')
 
 
-st.subheader('5. Qual é o impacto da escolaridade na expectativa de vida dos seres humanos?')
+st.subheader('5. Qual é o impacto da escolaridade na expectativa de vida dos seres humanos?', divider='grey')
+
 correlacaoEscolaridadeExpectativa = df[['Schooling', 'Life expectancy']].corr()
 st.write("A correlação entre a escolaridade e a expectativa de vida é: ")
 st.write(correlacaoEscolaridadeExpectativa)
 
-fig1, ax1 = plt.subplots()
-sns.scatterplot(x='Schooling', y='Life expectancy', data=df, ax=ax1)
+fig5, ax5 = plt.subplots()
+sns.scatterplot(x='Schooling', y='Life expectancy', data=df, ax=ax5)
 plt.title("Dispersão entre a escolaridade e a expectativa de vida")
 plt.xlabel("Escolaridade")
 plt.ylabel("Expectativa de vida")
 plt.xlim(-4, 20)
-st.pyplot(fig1)
+st.pyplot(fig5)
 
 st.write("Conclusões da pergunta 5:")
 st.write("Com base no coeficiente entre a expectativa de vida e a escolaridade, percebe-se que, " \
@@ -118,17 +148,25 @@ st.write("Com base no coeficiente entre a expectativa de vida e a escolaridade, 
 "demonstrando que um maior investimento na educação ajuda no crescimento da expectativa de vida.")
 
 
-st.subheader("6. A expectativa de vida tem relação positiva ou negativa com o consumo de álcool? ")
+st.subheader("6. A expectativa de vida tem relação positiva ou negativa com o consumo de álcool?", divider='grey')
 
-correlacaoAlcoolMortalidadeAdulta = df[['Alcohol', 'Adult Mortality']].corr()
-st.write("O coeficiente de correlação entre o consumo de alcool e a taxa de mortalidade adulta é: ")
-st.write(correlacaoAlcoolMortalidadeAdulta)
+correlacaoAlcoolExpectativa = df[['Alcohol', 'Life expectancy']].corr()
+st.write("A correlação entre a expectativa de vida e o consumo de alcool é: ")
+st.write(correlacaoAlcoolExpectativa)
 
-correlacaoAlcoolMortalidadeInfantil = df[['Alcohol', 'infant deaths']].corr()
-st.write("O coeficiente de correlação entre o consumo de alcool e a taxa de mortalidade infantil é: ")
-st.write(correlacaoAlcoolMortalidadeInfantil)
+fig6, ax6 = plt.subplots()
+sns.regplot(x='Alcohol', y='Life expectancy', data=df, ax=ax6, scatter_kws={"alpha":0.5})
+plt.title("Gráfico de dispersão para a expectativa de vida com o consumo de alcool")
+plt.xlabel("Alcool")
+plt.ylabel("Expectativa de vida")
+st.pyplot(fig6)
 
 st.write('Conclusões da pergunta 6:')
-st.write('Com base nos coeficientes demonstradas a cima, quanto mais proximo do negativo, menos ligação entre as variáveis exite. Portanto é evidente que o alcool não tem ligação direta com a expectativa de vida. '
+st.write('Com base no coeficiente demonstrado a cima, quanto mais proximo do 0, menos ligação entre as variáveis exite. Portanto é evidente que o alcool não tem ligação direta com a expectativa de vida. '
 'OBS: mesmo com dados lineares, não é possível alegar que o alcool não faz mal ao ser humano!')
 st.write('\n')
+
+
+# st.subheader("7. Países densamente povoados tendem a ter menor expectativa de vida?", divider='grey')
+# porPais = df.groupby('Country')['Life expectancy'].mean().sort_values()
+# print(porPais)
