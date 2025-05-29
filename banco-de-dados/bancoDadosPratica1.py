b34930146ea5f3f046b332efda676d306906cb22
HeitorDalla <heitorvillavilla@gmail.com>
2025-05-29 10:41:09 -0300

    Fazendo a pratica com a biblioteca datetime e criando as primeiras variaveis de execucao dentro da pratica do banco de dado

--- banco-de-dados/bancoDadosPratica1.py
+++ banco-de-dados/bancoDadosPratica1.py
@@ -1,7 +1,10 @@
 import sqlite3
 from collections import Counter
 from collections import defaultdict
+from datetime import date, time, datetime, timedelta, tzinfo, timezone
 import pandas as pd
 import streamlit as st
-from datetime import date, time, datetime, timedelta, tzinfo, timezone
+
+conn = sqlite3.connect('ecommerce.db') # Criando a conexão com banco de dados
+cursos = conn.cursor() # Executor de comandos
 
