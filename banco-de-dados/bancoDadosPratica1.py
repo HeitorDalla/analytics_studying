d93df3b0c16955edbe2d8ff42fa4738a3541bd9d
HeitorDalla <heitorvillavilla@gmail.com>
2025-05-30 15:39:40 -0300

    Adicionado pratica de graficos para estudo

--- banco-de-dados/bancoDadosPratica1.py
+++ banco-de-dados/bancoDadosPratica1.py
@@ -5,7 +5,7 @@ from datetime import date, time, datetime, timedelta, tzinfo, timezone
 import pandas as pd
 import streamlit as st
 
-conn = sqlite3.connect('ecommerce.db') # Criando a conexão com banco de dados
+conn = sqlite3.connect('banco-de-dados/ecommerce.db') # Criando a conexão com banco de dados
 cursor = conn.cursor() # Executor de comandos
 
 # Criando a tabela de autores
