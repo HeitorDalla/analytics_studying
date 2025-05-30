66cb6995fdb320b1c7a351a04d78d4cf8baa9c9c
HeitorDalla <heitorvillavilla@gmail.com>
2025-05-29 23:14:57 -0300

    Finalizado o exercicio 7 de analise grafica

--- analiseDados/pratica2/index.py
+++ analiseDados/pratica2/index.py
@@ -152,7 +152,6 @@ st.write("Com base no coeficiente entre a expectativa de vida e a escolaridade, 
 "demonstrando que um maior investimento na educação ajuda no crescimento da expectativa de vida.")
 
 
-st.subheader("6. A expectativa de vida tem relação positiva ou negativa com o consumo de álcool?", divider='grey')
 
 correlacaoAlcoolExpectativa = df[['Alcohol', 'Life expectancy']].corr()
 st.write("A correlação entre a expectativa de vida e o consumo de alcool é: ")
@@ -168,4 +167,18 @@ st.pyplot(fig6)
 st.write('Conclusões da pergunta 6:')
 st.write('Com base no coeficiente demonstrado a cima, quanto mais proximo do 0, menos ligação entre as variáveis exite. Portanto é evidente que o alcool não tem ligação direta com a expectativa de vida. '
 'OBS: mesmo com dados lineares, não é possível alegar que o alcool não faz mal ao ser humano!')
+st.write('\n')
+
+
+st.subheader("7. Países densamente povoados tendem a ter menor expectativa de vida?", divider='grey')
+
+fig7, ax7 = plt.subplots()
+sns.regplot(x='Population', y='Life expectancy', data=df, ax=ax7, scatter_kws={"alpha":0.2})
+plt.title("Gráfico de dispersão que relaciona a densidade da população e a expectativa de vida")
+plt.xlabel("Population")
+plt.ylabel("Expectativa de vida")
+st.pyplot(fig7)
+
+st.write('Conclusões da pergunta 7:')
+st.write('Com base na análise gráfica é possível observar que a densidade demográfica não afeta a expectativa de vida.')
 st.write('\n')
